488300c53709556ebca20fc787c3169f059ef9b3
MasudRana71 <tanzilmasud71@gmail.com>
2024-09-16 17:25:37 +0600

    final-update

--- styles/alpha-gamle/alpha.js
+++ styles/alpha-gamle/alpha.js
@@ -1,3 +1,33 @@
+
+
+
+document.addEventListener('keyup', handleKeyboardPress);
+function handleKeyboardPress(event){
+    const playerPressed =event.key;
+    if(playerPressed==='Escape'){
+        gameOver();
+    }
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+
+    if( playerPressed === expectedAlphabet){
+        const currentScore = getTextElementValueByID('current-score');
+        const newScore = currentScore + 1;
+        setTextElementValueByID('current-score', newScore);
+        removeBackgroundColorById(playerPressed);
+        continueGame();
+    }
+    else{
+        const currentLife = getTextElementValueByID('current-life');
+        const newLife = currentLife - 1;
+        setTextElementValueByID('current-life', newLife);
+        if(newLife===0){
+            gameOver();
+        }
+    }
+}
 function continueGame(){
     const alphabet = getRandomAlphabet();
     setBackgroundColorById(alphabet);
@@ -11,6 +41,21 @@ function continueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('total-score');
     showElementById('play-ground');
+
+    setTextElementValueByID('current-life', 5)
+    setTextElementValueByID('current-score', 0)
+
+
     continueGame();
+}
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('total-score');
+
+    const lastScore = getTextElementValueByID('current-score');
+    setTextElementValueByID('last-score', lastScore);
+    const removeAlphabet = getTextElementOntheBoard('current-alphabet');
+    removeBackgroundColorById(removeAlphabet);
 }
--- styles/alpha-gamle/utility.js
+++ styles/alpha-gamle/utility.js
@@ -5,12 +5,26 @@ function hideElementById(elementId){
 }
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
-    element.classList.add('bg-[#facc15]');
+    element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
 }
 function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 
+}
+function getTextElementValueByID(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+function setTextElementValueByID(elementId, value){
+    const element = document.getElementById(elementId);
+    element. innerText = value;
 }
 function getRandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
@@ -20,7 +34,11 @@ function getRandomAlphabet(){
     const index = Math.round(randomAlphabet);
 
     const alphabet = alphabets[index];
-    console.log( index, alphabet);
     return alphabet;
     
+}
+function getTextElementOntheBoard(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
